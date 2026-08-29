3bc2781d4d8b5cfc8be7bf4e37a0467945e5a9da
Muhammad Sobri Maulana <sobri3195@gmail.com>
2026-08-30 03:51:21 +0700

    feat: build offline-first mobile patient app

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+const CACHE='izin-dok-v1';
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['/','/apps','/manifest.webmanifest']))));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('/apps'))))});
